f71d34ab02bcd29e2968519846d277d1c97679e7
Jacob <schoen575@gmail.com>
2015-11-04 14:18:38 -0500

    code refactoring 2

--- blackjack.rb
+++ blackjack.rb
@@ -1,150 +1,129 @@
 def init_deck
-  suits = ['H', 'D', 'S', 'C']
-  cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  deck = suits.product(cards)
+  suits = %w(H D S C)
+  ranks = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  deck = suits.product(ranks)
   deck.shuffle!
 end
 
-def deal_card(participant_cards, deck)
-  participant_cards << deck.pop
+def deal_card(hand, deck)
+  hand << deck.pop
 end
 
-def format_cards(cards)
-  cards_string = ''
-  cards.each do |card|
-    cards_string << "  |#{card.join('-')}|"
+def format_cards(hand)
+  string = ''
+  hand.each do |card|
+    string << "  |#{card.join('-')}|"
   end
-  cards_string
+  string
 end
 
-def total_value(cards)
-  cards_values = {
-    '2' => 2,
-    '3' => 3,
-    '4' => 4,
-    '5' => 5,
-    '6' => 6,
-    '7' => 7,
-    '8' => 8,
-    '9' => 9,
-    '10' => 10,
-    'J' => 10,
-    'Q' => 10, 
-    'K' => 10,
-    'A' => 11
-  }
+def check_value(hand)
+  cards = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  values = [*(2..10), 10, 10, 10, 11]
+  card_values = Hash[cards.zip values]
 
-  second_part_of_cards = cards.map { |v| v[1] }
-  total = cards_values.values_at(*second_part_of_cards).inject(:+)
-  
-  second_part_of_cards.count('A').times do
-    total -= 10 if total > 21
+  rank = hand.map { |v| v[1] }
+  sum = card_values.values_at(*rank).inject(:+)
+
+  rank.count('A').times do
+    sum -= 10 if sum > 21
   end
-  total
+  sum
 end
 
-def display_table(player_name, dealers_cards, players_cards, state = {})
+def display_table(player_name, dealers_hand, players_hand, state = {})
   system 'clear'
+  dealers_score = check_value(dealers_hand)
+  players_score = check_value(players_hand)
   if state[:players_turn]
-    puts "Dealer's cards:  |--|#{format_cards([dealers_cards.last])}"
+    puts "Dealer's hand:  |--|#{format_cards([dealers_hand.last])}"
   else
-    puts "Dealer's cards:#{format_cards(dealers_cards)}"
-    puts "totaling to: #{total_value(dealers_cards)}"
+    puts "Dealer's hand:#{format_cards(dealers_hand)}"
+    puts "totaling to: #{dealers_score}"
   end
   puts '-' * 15
-  puts "#{player_name}'s cards:#{format_cards(players_cards)}"
-  puts "totaling to: #{total_value(players_cards)}"
-  if state[:game_over]
-    puts '-' * 15
-    puts "Results: #{results_msg(player_name, dealers_cards, players_cards)}"
+  puts "#{player_name}'s hand:#{format_cards(players_hand)}"
+  puts "totaling to: #{players_score}"
+  return unless state[:game_over]
+  puts '-' * 15
+  puts "Results: #{results_msg(player_name, dealers_score, players_score)}"
+end
+
+def blackjack?(score)
+  score == 21
+end
+
+def busted?(score)
+  score > 21
+end
+
+def results_msg(player_name, dealers_score, players_score)
+  case
+  when (dealers_score == players_score)
+    "It's a tie!"
+  when busted?(dealers_score)
+    "#{player_name} won! dealer has busted"
+  when busted?(players_score)
+    "oops! You've busted"
+  when blackjack?(dealers_score)
+    "#{player_name}, You lost. dealer hit blacjack"
+  when blackjack?(players_score)
+    "#{player_name}, You've hit blackjack"
+  when (dealers_score > players_score)
+    "#{player_name}, you lost"
+  else
+    "#{player_name} won!"
   end
 end
 
-def blackjack?(participant)
-  (total_value(participant) == 21) ? true : false
-end
-
-def busted?(participant)
-  (total_value(participant) > 21) ? true : false
-end
-
-def results_msg(player_name, dealer, player)
-  case 
-    when (total_value(dealer) == total_value(player))
-      "It's a tie!"
-    when busted?(dealer)
-      "#{player_name} won! dealer has busted"
-    when busted?(player)
-      "oops! You've busted"
-    when blackjack?(dealer)
-      "#{player_name}, You lost. dealer hit blacjack"
-    when blackjack?(player)
-      "#{player_name}, You've hit blackjack"
-    when (total_value(dealer) > total_value(player))
-      "#{player_name}, you lost"
-    else
-      "#{player_name} won!"
-  end
-end
-
-def game_over_msg(player_name, dealer, player)
-  system 'clear'
-  puts "Game over. Geting results..."
-  sleep 1
-  display_table(player_name, dealer, player, game_over: true)
-end
 
 
 puts 'Blackjack!'
-puts "How would you like to be called?"
+puts 'How would you like to be called?'
 player_name = gets.chomp
 
 loop do
-system 'clear'
-puts "Preparing the game..."
-sleep 1
-# game setup
-dealer = []
-player = []
-deck = init_deck
-2.times do
-  deal_card(dealer, deck)
-  deal_card(player, deck)
-end
-display_table(player_name, dealer, player, players_turn: true)
-# player's turn
-while total_value(player) < 21
-  puts "hit (h) or stay (s)"
-  input = gets.chomp.downcase
-  if !['s', 'h'].include?(input)
-    puts "We didn't get that..."
-    next
-  end
-  break if input == 's'
-  deal_card(player, deck)
-  display_table(player_name, dealer, player, players_turn: true)
-end
-# dealers turn
-if total_value(player) < 21
-  puts "You've chosen to stay. Turning to dealer"
+  system 'clear'
+  puts 'Preparing the game...'
   sleep 1
-  while total_value(dealer) < 17
-    puts "Dealer has #{total_value(dealer)} and will hit..."
-    sleep 1
-    display_table(player_name, dealer, player)
+  # game setup
+  dealer = []
+  player = []
+  deck = init_deck
+  2.times do
     deal_card(dealer, deck)
+    deal_card(player, deck)
   end
-end
-game_over_msg(player_name, dealer, player)
+  display_table(player_name, dealer, player, players_turn: true)
+  # player's turn
+  while check_value(player) < 21
+    puts "hit (h) or stay (s)"
+    input = gets.chomp.downcase
+    unless ['s', 'h'].include?(input)
+      puts "We didn't get that..."
+      next
+    end
+    break if input == 's'
+    deal_card(player, deck)
+    display_table(player_name, dealer, player, players_turn: true)
+  end
+  # dealers turn
+  if check_value(player) < 21
+    puts "You've chosen to stay. Turning to dealer"
+    sleep 1
+    while check_value(dealer) < 17
+      puts "Dealer has #{check_value(dealer)} and will hit..."
+      sleep 1
+      display_table(player_name, dealer, player)
+      deal_card(dealer, deck)
+    end
+  end
+  system 'clear'
+  puts 'Game over...'
+  sleep 1
+  display_table(player_name, dealer, player, game_over: true)
 
-puts "Play again? (Y/N)"
-break if gets.chomp.downcase == 'n'
+  puts 'Play again? (Y/N)'
+  break if gets.chomp.downcase == 'n'
 end
 puts 'Thanks for playing'
-
-
-
-
-
-
-
